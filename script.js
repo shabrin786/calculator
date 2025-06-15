b5fc52469fd74c14f5ed10dc56cb9b5f38c68aab
shabrin786 <shabirin786a@gmail.com>
2025-06-15 11:37:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,39 +6,61 @@ let expresion = "";
 btns.forEach((btn) => {
   const val = btn.innerText;
   btn.addEventListener("click", () => {
-    if (val === 'C') {
-      expresion = "";
-      inpute.value = "";
-    }
-
-    else if (btn.classList.contains('del')){
-      expresion = expresion.slice(0 , -1);
+    if(btn.classList.contains("del")){
+      expresion = expresion.slice(0, -1);
       inpute.value = expresion;
     }
-
-    else if (val === '=') {
-      try {
-        if (expresion.includes('%')) {
-          let [a, b] = expresion.split('%').map(Number);
-          expresion = ((a * b) / 100).toString();
-        } 
-        else {
-          expresion = eval(expresion).toString();
-        }
-        inpute.value = expresion;
-      } 
-      catch {
-        inpute.value = "Error";
-      }
+    else{
+      handleVal(val);
     }
+  });
+});
 
-    else {
-      expresion += val;
-      inpute.value = expresion;
-    }
+document.addEventListener("keydown" , (e) => {
+  const key = e.key;
 
-  })
+  if("0123456789+-./*%".includes(key)){
+    handleVal(key);
+  }
+  else if(key === "Enter"){
+    handleVal("=");
+  }
+  else if (key === "Backspace"){
+    expresion = expresion.slice(0 , -1);
+    inpute.value = expresion;
+  }
+  else if (key === "C"){
+    expresion = "";
+    inpute.value = "";
+  }
 });
 
 
+function handleVal(val) {
+  if (val === 'C') {
+    expresion = "";
+    inpute.value = "";
+  }
 
+  else if (val === '=') {
+    try {
+      if (expresion.includes('%')) {
+        let [a, b] = expresion.split('%').map(Number);
+        expresion = ((a * b) / 100).toString();
+      }
+      else {
+        expresion = eval(expresion).toString();
+      }
+      inpute.value = expresion;
+    }
+    catch {
+      inpute.value = "Error";
+    }
+  }
+
+  else {
+    expresion += val;
+    inpute.value = expresion;
+  }
+
+}
